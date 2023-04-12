1178c8e0de1d8097b3961dae09e0fc680c9a8e75
Pham Duc <ducphgcd191221@fpt.edu.vn>
2023-04-12 15:23:33 +0700

    feat: create member (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import MainLayouts from '@root/layouts/MainLayout'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import CreateMember from './components/CreateMember/Createmember'
 import TestStyle from './layouts/TestStyle'
 
 export default function App() {
@@ -25,6 +26,7 @@ export default function App() {
         <Route element={<Auth />} path='/login' />
         <Route exact element={<MainLayouts />} path='/dashboard' />
         <Route exact element={<TestStyle />} path='/teststyle' />
+        <Route element={<CreateMember />} path='createmember' />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/CreateMember/Createmember.jsx
@@ -0,0 +1,108 @@
+import { Card, Row, Col, Button, Form, Input } from 'antd'
+
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+    email: '${label} is not a valid email!',
+  },
+  number: {
+    range: '${label} must be between ${min} and ${max}',
+  },
+}
+export default function CreateMember() {
+  return (
+    <>
+      <div className='site-card-border-less-wrapper'>
+        <Card
+          bordered={true}
+          className='card-boxshadow'
+          style={{
+            width: '100%',
+          }}
+          title='Create Member'
+        >
+          <Row>
+            <Col span={8}>
+              <Form
+                initialValues={{ password: 'password' }}
+                layout={'vertical'}
+                validateMessages={validateMessages}
+              >
+                <Form.Item
+                  name={['password']}
+                  style={{ display: 'none' }}
+                ></Form.Item>
+                <Row>
+                  <Col span={12}>
+                    <Form.Item
+                      label='First Name'
+                      name={['firstName']}
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Please type first name!',
+                        },
+                      ]}
+                    >
+                      <Input
+                        placeholder={'First Name'}
+                        style={{ width: '90%' }}
+                      />
+                    </Form.Item>
+                  </Col>
+                  <Col span={12}>
+                    <Form.Item
+                      label='Last Name'
+                      name={['lastName']}
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Please type last name!',
+                        },
+                      ]}
+                    >
+                      <Input placeholder={'Last Name'} />
+                    </Form.Item>
+                  </Col>
+                </Row>
+
+                <Form.Item
+                  label='Email'
+                  name={['email']}
+                  rules={[
+                    {
+                      required: true,
+                      type: 'email',
+                    },
+                  ]}
+                >
+                  <Input placeholder={'Email'} />
+                </Form.Item>
+                <Form.Item
+                  label='Slack ID'
+                  name={['slackid']}
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please type slack id!',
+                    },
+                  ]}
+                >
+                  <Input min={1} placeholder={'Slack Id'} type='number' />
+                </Form.Item>
+                <Form.Item>
+                  <Button htmlType='submit' type='primary'>
+                    Submit
+                  </Button>
+                  <Button danger htmlType='cancel' type='primary'>
+                    Cancel
+                  </Button>
+                </Form.Item>
+              </Form>
+            </Col>
+          </Row>
+        </Card>
+      </div>
+    </>
+  )
+}
